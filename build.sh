ec853f25e67ee12801d7202850e8eb92c29e8b59
as <amirhesham957@gmail.com>
2026-05-14 07:17:10 +0300

    Final Vercel fix: No shaders, clean build

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
-
-# Clone Flutter if not present
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1 flutter
 fi
-
 export PATH="$PATH:`pwd`/flutter/bin"
-
-# Setup
-flutter precache --web
 flutter pub get
-
-# Build for web (no shaders, no web-renderer flag)
 flutter build web --release
